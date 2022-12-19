4daeaad709b1d9eadfb2909d4223e642a2485dd7
souravbhatia1997 <souravbhatia64@gmail.com>
2022-12-19 23:59:14 +0530

    changes in download logic and server connection

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require("path");
+const path = require('path');
 const YoutubeMp3Downloader = require('youtube-mp3-downloader');
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 const ffmpeg = require('fluent-ffmpeg');
@@ -8,19 +8,44 @@ var router = express.Router();
 
 router.post('/', (req, res) => {
   let payloadData = req.body;
-  console.log("payloadData:  ", payloadData);
+  let queryPattern = /\?/;
+  console.log('payloadData:  ', payloadData);
   let url = payloadData.url;
-  let splitedUrlArray = url.split("/");
-  let index = splitedUrlArray.length - 1;
-  let videoId = splitedUrlArray[index];
-  console.log("videoId: ", videoId);
+  // let videoIds = [];
+  let videoId = '';
 
+  if (queryPattern.test(url)) {
+    let splitUrlByEqual = url.split('=');
+    let eIndex = splitUrlByEqual.length - 1;
+    // videoIds.push(splitUrlByEqual[eIndex]);
+    videoId = splitUrlByEqual[eIndex];
+  } else {
+    let splitUrlBySlash = url.split('/');
+    let index = splitUrlBySlash.length - 1;
+    // videoIds.push(splitUrlBySlash[index]);
+    videoId = splitUrlBySlash[index];
+  }
+
+  // let result = false;
+  // console.log('videoIds: ', videoIds);
+  // const pattern = /^[a-zA-Z0-9-_]{11}$/;
+  // for (let i in videoIds) {
+  //   console.log('videoIds[i]:  ', videoIds[i]);
+  //   result = pattern.test(videoIds[i]);
+  //   if (result === true) {
+  //     videoId = videoIds[i];
+  //     break;
+  //   }
+  // }
+  console.log('videoId: ', videoId);
+
+  // if (result === true) {
   let YD = new YoutubeMp3Downloader({
-    outputPath: path.join(__dirname, "..", "public", "static", "mp3"),
+    outputPath: path.join(__dirname, '..', 'public', 'static', 'mp3'),
     youtubeVideoQuality: 'highestaudio',
     queueParallelism: 2,
     progressTimeout: 2000,
-    allowWebm: false
+    allowWebm: false,
   });
 
   YD.download(videoId);
@@ -36,6 +61,7 @@ router.post('/', (req, res) => {
   YD.on('progress', function (progress) {
     console.log(JSON.stringify(progress));
   });
+  // }
 });
 
 module.exports = router;
